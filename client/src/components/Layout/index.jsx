c98587eb9fc8315450d02557f1a8817fbfc75c8d
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-01-20 14:55:30 +0800

    feat: create contact

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -3,22 +3,35 @@ import Navbar from "../Navbar";
 
 function AppLayout() {
   return (
-    <div className="h-fit bg-cyan-950 py-5 overflow-x-hidden">
-      <Navbar />
-      <div className="divider mx-10 divider-info before:bg-gray-300 after:bg-gray-300"></div>
-      <div className="w-screen flex flex-col gap-3 items-center justify-center">
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
+    <>
+      <button className="btn btn-circle fixed bottom-5 right-5 z-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="#f9fafa"
+          viewBox="0 0 256 256"
+        >
+          <path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>
+        </svg>
+      </button>
+      <div className="h-fit bg-cyan-950 py-5 overflow-x-hidden">
+        <Navbar />
+        <div className="divider mx-10 divider-info before:bg-gray-300 after:bg-gray-300"></div>
+        <div className="w-screen flex flex-col gap-3 items-center justify-center">
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
