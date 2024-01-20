4f0fe09ae70f463c8435016eeff0985ec254c938
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-01-20 13:57:36 +0800

    feat: create router

---
+++ client/src/components/Layout/index.jsx
@@ -0,0 +1,9 @@
+function AppLayout() {
+  return (
+    <div className="w-screen bg-base-400">
+      <p>Hi</p>
+    </div>
+  );
+}
+
+export default AppLayout;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
