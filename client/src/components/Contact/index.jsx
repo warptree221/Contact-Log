d1ff6cff9b2482d0588a3109c652688d977b6e36
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-01-20 14:48:03 +0800

    feat: create contact

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -1,14 +1,22 @@
 export default function Contact() {
   return (
-    <div className="bg-cyan-900 flex justify-between h-16 w-[95%] px-10">
+    <div className="bg-cyan-900 flex justify-between h-16 w-[93%] px-6 hover:bg-cyan-800 hover:scale-[1.01] hover:shadow-slate-400 transition-all">
       <div className="flex gap-5 items-center">
-        <div className="rounded-full h-10 w-10 bg-red-300"></div>
+        <div className="avatar">
+          <div className="w-10 rounded-full">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
         <div className="flex flex-col">
-          <p className="text-md">Alice Adams</p>
+          <p className="text-md font-bold text-white">Alice Adams</p>
           <p className="text-sm">Officemate</p>
         </div>
       </div>
-      <p className="text-sm">Details</p>
+      <div className="flex items-center">
+        <button className="text-sm font-bold text-white hover:text-cyan-400">
+          Details
+        </button>
+      </div>
     </div>
   );
 }
--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -3,10 +3,19 @@ import Navbar from "../Navbar";
 
 function AppLayout() {
   return (
-    <div className="h-fit bg-cyan-950 py-5">
+    <div className="h-fit bg-cyan-950 py-5 overflow-x-hidden">
       <Navbar />
       <div className="divider mx-10 divider-info before:bg-gray-300 after:bg-gray-300"></div>
-      <div className="w-screen flex items-center justify-center">
+      <div className="w-screen flex flex-col gap-3 items-center justify-center">
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
         <Contact />
       </div>
     </div>
